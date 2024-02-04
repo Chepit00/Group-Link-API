71d16d385aaca2ed5a777f7d0a89a63c194e3401
Jose Silva <js38902@gmail.com>
2024-02-04 18:24:30 -0500

    Added Thoughts.js

---
+++ models/Thoughts.js
@@ -0,0 +1,32 @@
+const { Schema } = require("mongoose");
+
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: stringify,
+    required: true,
+    maxlength: 280,
+    minlength: 1,
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now,
+    //Add getter function add here 
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+  }
+);
+
+
+//virtual called reationCount that retrieves length of thoughts reactions array field on query
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+module.exports
