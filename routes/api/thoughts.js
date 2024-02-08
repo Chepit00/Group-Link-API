26d251d95cf2a247680a9ebee79df68e27c9fa13
Jose Silva <js38902@gmail.com>
2024-02-07 22:41:34 -0500

    Added user and thought API routes

---
+++ routes/api/thoughts.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    newThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+//gets all thoughts, creates new thought 
+router.route('/').get(getThoughts).post(newThought);
+
+//get a single thought by ID, updates a thought via ID, deletes thought by ID
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+//create a reaction, delete a reaction via reactionID
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
